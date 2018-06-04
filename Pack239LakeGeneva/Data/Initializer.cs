0a15997d33f671c5f950805ae3c6dd0e3d9b3295
Neil Doebler <NeilDoebler@outlook.com>
2018-06-03 19:56:50 -0500

    Initial creation of site

---
+++ Pack239LakeGeneva/Data/Initializer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pack239LakeGeneva.Data
+{
+    public static class Initializer
+    {
+      public static async Task initialize(RoleManager<IdentityRole> roleManager)
+      {
+        if (!await roleManager.RoleExistsAsync("Admin"))
+      {
+        var users = new IdentityRole("Admin");
+        await roleManager.CreateAsync(users);
+      }
+
+      if (!await roleManager.RoleExistsAsync("User"))
+      {
+        var users = new IdentityRole("User");
+        await roleManager.CreateAsync(users);
+      }
+
+      if (!await roleManager.RoleExistsAsync("Manager"))
+      {
+        var users = new IdentityRole("Manager");
+        await roleManager.CreateAsync(users);
+      }
+    }
+    }
+}
---
+++ Pack239LakeGeneva/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pack239LakeGeneva.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
