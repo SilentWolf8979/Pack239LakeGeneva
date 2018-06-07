b14a5c2a38588276481d0dea433073189a3dac91
Doebler, Neil <Neil.Doebler@allstate.com>
2018-06-07 08:00:33 -0500

    Remove bower and use npm

---
+++ Pack239LakeGeneva/gulpfile.js
@@ -0,0 +1,32 @@
+﻿/// <binding AfterBuild='copy-bootstrap, copy-jquery' />
+var gulp = require('gulp')
+
+gulp.task('copy-bootstrap', function ()
+{
+  try
+  {
+    gulp.src('node_modules/bootstrap/dist/css/*.css')
+      .pipe(gulp.dest('wwwroot/lib/css/bootstrap'));
+
+    gulp.src('node_modules/bootstrap/dist/js/*.js')
+      .pipe(gulp.dest('wwwroot/lib/js/bootstrap'));
+  }
+  catch (e)
+  {
+    return -1;
+  }
+  return 0;
+});
+
+gulp.task('copy-jquery', function () {
+  try
+  {
+    gulp.src('node_modules/jquery/dist/*.js')
+      .pipe(gulp.dest('wwwroot/lib/js/jquery'));
+  }
+  catch (e)
+  {
+    return -1;
+  }
+  return 0;
+});
