785c4dcc2f8b2762878af832e3dabdb2969991ed
Neil Doebler <NeilDoebler@outlook.com>
2018-08-23 18:48:59 -0500

    Updating error page

--- Pack239LakeGeneva/Controllers/HomeController.cs
+++ Pack239LakeGeneva/Controllers/HomeController.cs
@@ -27,7 +27,7 @@ namespace Pack239LakeGeneva.Controllers
 
     public IActionResult Error()
     {
-      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier, StatusCode = HttpContext.Response.StatusCode });
     }
   }
 }
--- Pack239LakeGeneva/Models/ErrorViewModel.cs
+++ Pack239LakeGeneva/Models/ErrorViewModel.cs
@@ -2,10 +2,11 @@ using System;
 
 namespace Pack239LakeGeneva.Models
 {
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+    public int StatusCode { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
 }
