d6a81cdb2b35a812c9dd60efb999cd976ea1584d
SilentWolf8979 <NeilDoebler@outlook.com>
2018-09-28 14:00:19 -0500

    Updating gulpfile for Gulp 4

--- Pack239LakeGeneva/gulpfile.js
+++ Pack239LakeGeneva/gulpfile.js
@@ -4,75 +4,54 @@ var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify-es').default;
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('copy-bootstrap', function ()
-{
-  try
-  {
-    gulp.src('node_modules/bootstrap/dist/css/*.css')
-      .pipe(cleanCSS({ sourceMap: true, processImport: false }))
-      .pipe(gulp.dest('wwwroot/lib/css/bootstrap'));
+gulp.task('copy-bootstrap-css', function () {
+  return gulp.src('node_modules/bootstrap/dist/css/*.css')
+    .pipe(cleanCSS({ sourceMap: true, processImport: false }))
+    .pipe(gulp.dest('wwwroot/lib/css/bootstrap'));
+});
 
-    gulp.src('node_modules/bootstrap/dist/js/*.js')
-      .pipe(sourcemaps.init())
-      .pipe(uglify())
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('wwwroot/lib/js/bootstrap'));
-  }
-  catch (e)
-  {
-    return -1;
-  }
-  return 0;
+gulp.task('copy-bootstrap-js', function () {
+  return gulp.src('node_modules/bootstrap/dist/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('wwwroot/lib/js/bootstrap'));
 });
 
 gulp.task('copy-jquery', function () {
-  try
-  {
-    gulp.src('node_modules/jquery/dist/*.js')
-      .pipe(sourcemaps.init())
-      .pipe(uglify())
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('wwwroot/lib/js/jquery'));
-  }
-  catch (e)
-  {
-    return -1;
-  }
-  return 0;
+  return gulp.src('node_modules/jquery/dist/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('wwwroot/lib/js/jquery'));
 });
 
-gulp.task('copy-fontawesome', function () {
-  try {
-    gulp.src('wwwroot/css/fontawesome/*.css')
-      .pipe(cleanCSS({ sourceMap: true, processImport: false }))
-      .pipe(gulp.dest('wwwroot/lib/css/fontawesome'));
-
-    gulp.src('wwwroot/js/fontawesome/*.js')
-      .pipe(sourcemaps.init())
-      .pipe(uglify())
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('wwwroot/lib/js/fontawesome'));
-  }
-  catch (e) {
-    return -1;
-  }
-  return 0;
+gulp.task('copy-fontawesome-css', function () {
+  return gulp.src('wwwroot/css/fontawesome/*.css')
+    .pipe(cleanCSS({ sourceMap: true, processImport: false }))
+    .pipe(gulp.dest('wwwroot/lib/css/fontawesome'));
 });
 
-gulp.task('minify-site', function () {
-  try {
-    gulp.src('wwwroot/css/*.css')
-      .pipe(cleanCSS({ sourceMap: true, processImport: false }))
-      .pipe(gulp.dest('wwwroot/lib/css'));
-
-    gulp.src('wwwroot/js/*.js')
-      .pipe(sourcemaps.init())
-      .pipe(uglify())
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('wwwroot/lib/js'));
-  }
-  catch (e) {
-    return -1;
-  }
-  return 0;
+gulp.task('copy-fontawesome-js', function () {
+  return gulp.src('wwwroot/js/fontawesome/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('wwwroot/lib/js/fontawesome'));
 });
+
+gulp.task('minify-site-css', function () {
+  return gulp.src('wwwroot/css/*.css')
+    .pipe(cleanCSS({ sourceMap: true, processImport: false }))
+    .pipe(gulp.dest('wwwroot/lib/css'));
+});
+
+gulp.task('minify-site-js', function () {
+  return gulp.src('wwwroot/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('wwwroot/lib/js'));
+});
+
+gulp.task('default', gulp.parallel('copy-bootstrap-css', 'copy-bootstrap-js', 'copy-jquery', 'copy-fontawesome-css', 'copy-fontawesome-js', 'minify-site-css', 'minify-site-js'));
